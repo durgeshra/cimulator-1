6458730b3cab8b13a7a6b497cd2033fc53a09156
Durgesh Agrawal <33284183+durgeshra@users.noreply.github.com>
2018-09-27 00:23:58 +0530

    Add test (#4)
    * HW
    
    * Add comma
    
    * Rename file
    
    * Add test
    
    * Resolve conflict
    
    * Add test
    
    * Refine test
    
    * Refine test

---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func TestFour(t *testing.T) {
+	if got,want := four(),4; got != want {
+		t.Errorf("four() returns incorrect value. Got: %v, Want: %v", got, want)
+	}
+}
