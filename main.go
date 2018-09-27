923b9d737a457ea3cdf34805358056121be1a79a
cimulator <43617437+cimulator@users.noreply.github.com>
2018-09-27 09:13:24 -0700

    Update .travis.yml to include goimports check. (#6)
    * Update .travis.yml to include goimports check.
    
    * Add installation step for goimports.
    
    * Change the dirty detection in the .travis.yml
    
    * Fix the goimports path in .travis.yml
    
    * Format files before pushing them.
    
    * Use git diff command to show diff and exit.
    
    * Test with incorrect formatting in files.
    
    * Test with incorrect formatting in files (2).
    
    * Revert (finally) to all correct files.

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 // four justs returns 4.
 func four() int {
-   return 4
+	return 4
 }
 
 func main() {
-   fmt.Printf("Hello, world!")
+	fmt.Printf("Hello, world!")
 }
--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFour(t *testing.T) {
-	if got,want := four(),4; got != want {
+	if got, want := four(), 4; got != want {
 		t.Errorf("four() returns incorrect value. Got: %v, Want: %v", got, want)
 	}
 }
