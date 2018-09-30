6b0ede08cb4a75c7437e4c3a61b6472d3438c93b
Durgesh Agrawal <durgesha@iitk.ac.in>
2018-09-30 10:52:16 +0530

    Add file i/o feature

--- main.go
+++ main.go
@@ -15,13 +15,19 @@ func four() int {
 func main() {
 	fmt.Printf("Hello, world!\n")
 
-	data, err := ioutil.ReadFile(os.Args[1])
+	var data []byte
+	var err error
+
+	if len(os.Args) == 2 {
+		data, err = ioutil.ReadFile(os.Args[1])
+	} else {
+		log.Fatalf("Number of commands should be 2, is %d", len(os.Args))
+	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("File read successfully.\nContents:\n")
-	fmt.Printf(string(data))
+	fmt.Printf("File read successfully.\nContents:\n%s", string(data))
 
 }
